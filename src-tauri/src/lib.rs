790f18175a72611bef1b922566dd02235e6bb7c7
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2025-09-14 19:23:52 +0100

    feat: web server

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,29 @@
+use tauri::path::BaseDirectory;
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            if tauri::is_dev() {
+                return Ok(()) // Vite will already host a development server
+            }
+
+            let resource_path = app.path().resolve("web", BaseDirectory::Resource)?;
+
+            tauri::async_runtime::spawn(async move {
+                let server = rocket::fs::FileServer::from(resource_path);
+
+                rocket::build()
+                    .configure(rocket::Config::figment().merge(("port", 2420)).merge(("address", local_ip_address::local_ip().unwrap())))
+                    .mount("/", server)
+                    .launch()
+                    .await
+                    .expect("failed to launch Rocket server");
+            });
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
